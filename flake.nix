7aa3193e7d61e1ce4883878db49308fd7ae2b52b
alexpaniman <alexpaniman@gmail.com>
2024-06-08 16:47:58 +0300

    feature: integrate nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "axp :: c++ support library";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+  {
+
+    packages.${system}.default = pkgs.clangStdenv.mkDerivation {
+      name = "axp";
+      src = ./.;
+
+      nativeBuildInputs = with pkgs; [ clang-tools ];
+      buildInputs = with pkgs; [];
+    };
+
+  };
+}
